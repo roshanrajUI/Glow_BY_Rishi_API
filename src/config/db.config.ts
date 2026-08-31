874c4c38afc0cea5b59923368e51db2013473279
Roshan <roshanraj.ui499@gmail.com>
2026-09-01 00:30:22 +0530

    fix: remove default values from database configuration for environment consistency

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -12,11 +12,11 @@ import { Otp } from "../models/entities/otp";
 
 const dbConfig: DataSource = new DataSource({
   type: "mysql",
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "admin",
-  database: process.env.DB_DATABASE || "glow_by_rishi",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   timezone: "+05:30",
   entities: [
     User,
