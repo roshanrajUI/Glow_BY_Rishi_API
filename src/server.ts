d60af14fce386728f086a8123e56f3cf8df9ffaf
Roshan <roshanraj.ui499@gmail.com>
2026-09-06 19:59:15 +0530

    feat: update uploads endpoint to include API prefix

--- src/server.ts
+++ src/server.ts
@@ -44,7 +44,10 @@ async function startServer() {
       "/swagger.json",
       express.static(path.resolve(process.cwd(), "public")),
     );
-    app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+    app.use(
+      "/api/uploads",
+      express.static(path.join(process.cwd(), "uploads")),
+    );
     // Routes
     app.use("/api/services", myServicesRouter);
     app.use("/api/my-works", myWorkRouter);
