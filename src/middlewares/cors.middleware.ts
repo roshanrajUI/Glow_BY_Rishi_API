11a4d73437eb54a5c8a37549c6ba97e9bb01f93f
Roshan <roshanraj.ui499@gmail.com>
2026-09-16 23:04:36 +0530

    feat: update CORS middleware to include additional allowed origins

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -4,11 +4,15 @@ const allowedOrigins = (req: Request, res: Response, next: NextFunction) => {
   const allowOrigins = [
     "localhost:4200",
     "localhost:8000",
+    "glowbyrishi.in",
+    "https://glowbyrishi.in",
     "http://localhost:4200",
     "http://localhost:8000",
     "http://localhost:8000/api",
     "https://glow-by-rishi-api.onrender.com/api",
     "https://glow-by-rishi-ui.onrender.com",
+    "https://api.glowbyrishi.in",
+    "https://api.glowbyrishi.in/api",
   ];
   const origin = req.headers.origin!;
   const host = req.headers.host!;
